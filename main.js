e2e3cc47c9a0ad3b763168e6bd2e2f4ef7599de8
Phoebe Quincy <phoebequincy@gmail.com>
2018-10-27 23:13:04 -0600

    added header, centered color-picker

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     let grid = document.querySelector('.pixel-grid')
-    let numTiles = 300
+    let numTiles = 200
 
 
     for (let i = 0; i < numTiles; i++) {
@@ -20,22 +20,10 @@ document.addEventListener('DOMContentLoaded', function () {
     colorPicker.addEventListener("change", watchColorPicker, false);
 
     function watchColorPicker(event) {
-      document.querySelectorAll("p").forEach(function(p) {
+        document.querySelectorAll("p").forEach(function(p) {
         p.style.color = event.target.value;
-      });
+        });
     }
-    // let palette = document.querySelector('.palette')
-    // let numColors = 10
-    //
-    //
-    // for (let i = 0; i < numColors; i++) {
-    //
-    //   let color = document.createElement('div')
-    //   color.style.width = "10%"
-    //   color.style.paddingBottom = "10%";
-    //   color.style.border = "1px solid #D3D3D3"
-    //   color.style.float = "left"
-    //   palette.appendChild(color)
-    // }
+
 
  })
